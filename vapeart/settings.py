8efd8da2bf532b02944d229bae756ba969cf5069
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-18 04:46:04 +0400

    finished

--- vapeart/settings.py
+++ vapeart/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'vapeart.wsgi.application'
 
 # Database
 import time
-time.sleep(2)
+time.sleep(5)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
