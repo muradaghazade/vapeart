68e7770cbb8102a81f49a2ebe306a99535036351
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-18 04:51:50 +0400

    finished

--- vapeart/settings.py
+++ vapeart/settings.py
@@ -87,18 +87,18 @@ time.sleep(2)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
-            'USER': os.getenv('POSTGRES_USER', 'taskilled'),
-            'PORT': os.getenv('POSTGRES_PORT', 5432),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
-            'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
+    #         'USER': os.getenv('POSTGRES_USER', 'taskilled'),
+    #         'PORT': os.getenv('POSTGRES_PORT', 5432),
+    #         'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+    #         'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+    #     }
 }
 
 
